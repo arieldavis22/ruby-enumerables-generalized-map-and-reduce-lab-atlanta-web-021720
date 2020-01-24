67a042d533ac58c3a9b60684834374af63fcea53
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 00:23:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,15 +13,8 @@ end
 
 map([]) { |n| n * -1}
 
-def reduce(source_array, starting_point=0)
+def reduce(source_array, sv=0)
 
-  i = 0
-  total = 0
-  while i < source_array.length do
-    total += source_array[i]
-    i += 1
-  end
-  yield(total, starting_point)
 end
 
 # reduce([], 0) { |memo, n| memo + n}
