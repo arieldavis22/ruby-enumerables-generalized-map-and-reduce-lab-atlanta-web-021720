ed4c5cb8f063618c8bd6cce6d6d1e4257dcc2e7a
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 00:16:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+def map(source_array)
+
+  arr = []
+  i = 0
+  while i < source_array.length do
+    arr << yield(source_array[i])
+    i += 1
+  end
+arr
+end
+
+map([]) { |n| n * -1}
+
+def reduce(source_array, starting_point=0)
+
+  i = 0
+  total = 0
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  yield(total, starting_point)
+end
+
+reduce([], 0) { |memo, n| memo + n}
+
+reduce([], 0) {|memo, n| memo && n}
